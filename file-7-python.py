da0cdff426c93ee57af927c47ceb59753c59f8d8
sajidshaik18 <162982281+sajidshaik18@users.noreply.github.com>
2024-03-19 09:12:48 +0530

    Add files via upload

---
+++ file-7-python.py
@@ -0,0 +1,83 @@
+# ----> Assesment
+# ----> Assesment
+# 1.) dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+# dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+# Merge two python dictionary
+# o/p --> {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+# 2.)Python Program to determine if 
+# the given Sets Are Disjoint 
+# or Not without Using Inbuilt-in Functions
+
+# set1 = {'Python', 'Java', 'Data Science'}
+# set2 = {'ML', 'AI', 'R Language', 'Python'}
+# set3 = {'Data Analytics', 'Robotics', 'Deep Learning', 'ml'}
+# c = 0
+# flag=0
+# for val in ranges(3):
+#    c=c+1
+#    if c==1:
+#        for val in set1:
+#            if val in set2 or val in set3:
+#                 flag=1
+#                 break
+
+#    if c==2:
+#        for val in set2:
+#            if val in set1 or val in set3:
+#                 flag=1
+#                 break
+#    if c==3:
+#        for val in set3:
+#            if val in set2 or val in set1:
+#                 flag=1
+#                 break
+
+# if flag==0:
+#     print("disjoint")
+# else:
+#     print("joint")
+
+# 3.)
+list1 = ["m", "na", "i", "ke",]
+list2 = ["y", "me", "s", "lly"]
+
+# l3 = []
+# for val in range (len(kist1)):
+#     ans = list1[val]+list2[val]:
+
+
+
+
+
+
+
+
+
+
+# O/P --> ['my', 'name', 'is', 'kelly']
+
+# # ! or
+! c = 0
+# for val in set1, set2:
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3.)list1 = ["M", "na", "i", "Ke"]
+# list2 = ["y", "me", "s", "lly"]
+
+# O/P --> ['My', 'name', 'is', 'Kelly']
+# d1={'Ten':10,'Twenty':20,'Thirty':30}
+# d2={'Thirty':30,'fourty':40,'fifty':50}
+# d1.update(d2)
+# print(d1)
